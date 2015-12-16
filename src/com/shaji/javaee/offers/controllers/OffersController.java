6f839be8d6cc53e3140319632b06cfb2a15cbb73
Shaji Sarangadharan <sshaji@gmail.com>
2015-12-16 12:03:33 -0500

    Updated to working spring web app with Dispatcher Servlet, Controller and View Resolver

---
+++ src/com/shaji/javaee/offers/controllers/OffersController.java
@@ -0,0 +1,12 @@
+package com.shaji.javaee.offers.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OffersController {
+	@RequestMapping("/")
+	public String showHome() {
+		return "home";
+	}
+}
