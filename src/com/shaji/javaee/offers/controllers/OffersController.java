0a6ada9b5c8dde393e0b8f2f4556a232bdb63095
Shaji Sarangadharan <sshaji@gmail.com>
2015-12-16 12:38:28 -0500

    Added JSTL to display data passed from Controller in jsp

--- src/com/shaji/javaee/offers/controllers/OffersController.java
+++ src/com/shaji/javaee/offers/controllers/OffersController.java
@@ -1,12 +1,14 @@
 package com.shaji.javaee.offers.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OffersController {
 	@RequestMapping("/")
-	public String showHome() {
+	public String showHome(Model model) {
+		model.addAttribute("name", "Shaji");
 		return "home";
 	}
 }
