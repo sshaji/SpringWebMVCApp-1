c2589610c41a98a8762a49b8b516312f205abe03
Shaji Sarangadharan <sshaji@gmail.com>
2015-12-17 13:37:32 -0500

    Added mysql db "offers" using jdbc connection, and implemented REST calls

---
+++ src/com/shaji/javaee/offers/model/OffersDAO.java
@@ -0,0 +1,125 @@
+package com.shaji.javaee.offers.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component("offersDao")
+public class OffersDAO {
+
+	private NamedParameterJdbcTemplate jdbc;
+
+	@Autowired
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+	}
+
+	/**
+	 * Get all offers
+	 * 
+	 * @return
+	 */
+	public List<Offer> getOffers() {
+		String sql = "select * from offers";
+
+		return jdbc.query(sql, new RowMapper<Offer>() {
+
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Offer offer = new Offer();
+				offer.setId(rs.getInt("id"));
+				offer.setName(rs.getString("name"));
+				offer.setEmail(rs.getString("email"));
+				offer.setOfferDetails(rs.getString("offerdetails"));
+				return offer;
+			}
+
+		});
+	}
+
+	/**
+	 * Get one offer by id
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Offer getOfferById(int id) {
+		MapSqlParameterSource paramMap = new MapSqlParameterSource("id", id);
+		String sql = "select * from offers where id = :id";
+
+		return jdbc.queryForObject(sql, paramMap, new RowMapper<Offer>() {
+
+			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Offer(rs.getInt("id"), rs.getString("name"), rs.getString("email"),
+						rs.getString("offerdetails"));
+			}
+
+		});
+	}
+
+	/**
+	 * Create an offer
+	 * 
+	 * @param offer
+	 * @return
+	 */
+	public int createOffer(Offer offer) {
+		BeanPropertySqlParameterSource paramSource = new BeanPropertySqlParameterSource(offer);
+		String sql = "insert into offers (name, email, offerdetails) values (:name, :email, :offerDetails)";
+
+		return jdbc.update(sql, paramSource);
+	}
+
+	/**
+	 * Update an offer
+	 * 
+	 * @param offer
+	 * @return
+	 */
+	public int updateOffer(Offer offer) {
+		BeanPropertySqlParameterSource paramSource = new BeanPropertySqlParameterSource(offer);
+		String sql = "update offers set name = :name, email = :email, offerdetails = :offerDetails where id = :id";
+
+		return jdbc.update(sql, paramSource);
+	}
+
+	/**
+	 * Create multiple offers - Transaction
+	 * 
+	 * @param offers
+	 * @return
+	 */
+	@Transactional
+	public int[] createOffers(List<Offer> offers) {
+		SqlParameterSource[] batchParams = SqlParameterSourceUtils.createBatch(offers.toArray());
+		String sql = "insert into offers (id, name, email, offerdetails) values (:id, :name, :email, :offerDetails)";
+
+		return jdbc.batchUpdate(sql, batchParams);
+	}
+
+	/**
+	 * Update multiple offers - Transaction
+	 * 
+	 * @param offers
+	 * @return
+	 */
+	@Transactional
+	public int[] updateOffers(List<Offer> offers) {
+		SqlParameterSource[] batchParams = SqlParameterSourceUtils.createBatch(offers.toArray());
+		String sql = "update offers set name = :name, email = :email, offerdetails = :offerDetails where id = :id";
+
+		return jdbc.batchUpdate(sql, batchParams);
+	}
+
+}
--- src/com/shaji/javaee/offers/restcontrollers/OfferRestAPIController.java
+++ src/com/shaji/javaee/offers/restcontrollers/OfferRestAPIController.java
@@ -1,9 +1,9 @@
 package com.shaji.javaee.offers.restcontrollers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shaji.javaee.offers.model.User;
+import com.shaji.javaee.offers.model.Offer;
+import com.shaji.javaee.offers.model.OffersDAO;
 
 @RestController
 @RequestMapping(value = "/v1")
@@ -23,8 +24,12 @@ public class OfferRestAPIController {
 	 * @return
 	 */
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
-	public @ResponseBody List<User> get() {
-		return new ArrayList<User>(Arrays.asList(new User(1, "User1"), new User(2, "User2")));
+	public @ResponseBody List<Offer> get() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/shaji/javaee/offers/beans/beans.xml");
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
+		List<Offer> retOffers = offersDao.getOffers();
+		((ClassPathXmlApplicationContext) context).close();
+		return retOffers;
 	}
 
 	/**
@@ -34,8 +39,12 @@ public class OfferRestAPIController {
 	 * @return
 	 */
 	@RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
-	public @ResponseBody User get(@PathVariable("id") int id) {
-		return new User(id, "User");
+	public @ResponseBody Offer get(@PathVariable("id") int id) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/shaji/javaee/offers/beans/beans.xml");
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
+		Offer retOffer = offersDao.getOfferById(id);
+		((ClassPathXmlApplicationContext) context).close();
+		return retOffer;
 	}
 
 	/**
@@ -45,8 +54,31 @@ public class OfferRestAPIController {
 	 * @return
 	 */
 	@RequestMapping(value = "/users", method = RequestMethod.POST)
-	public @ResponseBody User post(@RequestBody User user) {
-		return user;
+	public @ResponseBody Offer post(@RequestBody Offer offer) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/shaji/javaee/offers/beans/beans.xml");
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
+		Offer retOffer = null;
+		offersDao.createOffer(offer);
+		retOffer = offer;
+		((ClassPathXmlApplicationContext) context).close();
+		return retOffer;
+	}
+
+	/**
+	 * Update one
+	 * 
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping(value = "/users", method = RequestMethod.PUT)
+	public @ResponseBody Offer put(@RequestBody Offer offer) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/shaji/javaee/offers/beans/beans.xml");
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
+		Offer retOffer = null;
+		offersDao.updateOffer(offer);
+		retOffer = offer;
+		((ClassPathXmlApplicationContext) context).close();
+		return retOffer;
 	}
 
 	/**
@@ -56,7 +88,13 @@ public class OfferRestAPIController {
 	 * @return
 	 */
 	@RequestMapping(value = "/users/masscreate", method = RequestMethod.POST)
-	public @ResponseBody List<User> post(@RequestBody List<User> users) {
-		return users;
+	public @ResponseBody List<Offer> post(@RequestBody List<Offer> offers) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/shaji/javaee/offers/beans/beans.xml");
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
+		List<Offer> retOffers = null;
+		offersDao.createOffers(offers);
+		retOffers = offers;
+		((ClassPathXmlApplicationContext) context).close();
+		return retOffers;
 	}
 }
